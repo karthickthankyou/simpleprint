753c0a72780468089af4cf60a8515fbf1b8cb2ed
Karthick Ragavendran <karthickthankyou@gmail.com>
2023-11-25 12:50:11 +0530

    update install

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
+#!/bin/bash
+
 INSTALL_DIR="/usr/local/bin"
 BINARY_URL="https://github.com/karthickthankyou/simpleprint/releases/download/v1.0.0/simpleprint"
 
 # Download the binary
-curl -sSL -o "$INSTALL_DIR/simpleprint.zip" "$BINARY_URL"
-
-# Unzip and remove the zip file
-unzip "$INSTALL_DIR/simpleprint.zip" -d "$INSTALL_DIR"
-rm "$INSTALL_DIR/simpleprint.zip"
+sudo curl -sSL -o "$INSTALL_DIR/simpleprint" "$BINARY_URL"
 
 # Make the binary executable
-chmod +x "$INSTALL_DIR/simpleprint"
+sudo chmod +x "$INSTALL_DIR/simpleprint"
+
+echo "Installation completed. You can now use SimplePrint with the command 'simpleprint'."
