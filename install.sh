69c001eb98516dd1fba75276701afbaabe4bed96
Karthick Ragavendran <karthickthankyou@gmail.com>
2023-11-25 12:38:52 +0530

    update install script

--- install.sh
+++ install.sh
@@ -1,23 +1,12 @@
-#!/bin/bash
-
-# Define the installation directory
 INSTALL_DIR="/usr/local/bin"
+BINARY_URL="https://github.com/karthickthankyou/simpleprint/releases/download/v1.0.0/simpleprint"
 
-# Compiling the Rust project
-echo "Compiling SimplePrint..."
-cargo build --release
+# Download the binary
+curl -sSL -o "$INSTALL_DIR/simpleprint.zip" "$BINARY_URL"
 
-# Check if the build was successful
-if [ ! -f target/release/simpleprint ]; then
-    echo "Build failed, cannot install."
-    exit 1
-fi
-
-# Copy the binary to the installation directory
-echo "Installing SimplePrint..."
-sudo mv target/release/simpleprint $INSTALL_DIR/simpleprint
+# Unzip and remove the zip file
+unzip "$INSTALL_DIR/simpleprint.zip" -d "$INSTALL_DIR"
+rm "$INSTALL_DIR/simpleprint.zip"
 
 # Make the binary executable
-sudo chmod +x $INSTALL_DIR/simpleprint
-
-echo "Installation completed. You can now use SimplePrint with the command 'simpleprint'."
+chmod +x "$INSTALL_DIR/simpleprint"
