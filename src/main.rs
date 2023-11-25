53e612fde489f20de73a1c94af9f2f36efa014e3
Karthick Ragavendran <karthickthankyou@gmail.com>
2023-11-25 14:18:24 +0530

    enforce sp extension

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,18 @@
 use std::fs;
+use std::path::Path;
+use std::env;
 
 fn main() {
-    let filename = std::env::args().nth(1).expect("No file provided");
-    let content = fs::read_to_string(filename).expect("Could not read file");
+    let filename = env::args().nth(1).expect("No file provided");
 
+    // Check if the file has the .sp extension
+    let path = Path::new(&filename);
+    if path.extension().unwrap_or_default() != "sp" {
+        eprintln!("Invalid file type. Please use a '.sp' file.");
+        return;
+    }
+
+    let content = fs::read_to_string(&filename).expect("Could not read file");
     interpret(&content);
 }
 
